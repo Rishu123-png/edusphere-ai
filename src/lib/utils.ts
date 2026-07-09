266db13e0c3cd0fb9ef04d778ebe2f0e1db6e38a
Rishu123-png <jaswarrishu642@gmail.com>
2026-07-09 20:48:59 +0530

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const generateSchoolCode = () => {
+  const chars = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
+  return "EDU-" + Array.from({length:4}, ()=>chars[Math.floor(Math.random()*chars.length)]).join('');
+}
+
+export const todayISO = () => new Date().toISOString().slice(0,10);

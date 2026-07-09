27ffa3f21c09bb4b094cd1b13032379c96d33764
Rishu123-png <jaswarrishu642@gmail.com>
2026-07-09 20:56:17 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { Toaster } from 'sonner'
-import App from './App'
+import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
+import { ThemeProvider } from './contexts/ThemeContext'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'sonner'
+
+const qc = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <Toaster position="top-center" richColors closeButton />
-    </BrowserRouter>
+    <QueryClientProvider client={qc}>
+      <ThemeProvider>
+        <AuthProvider>
+          <BrowserRouter>
+            <App />
+            <Toaster richColors position="top-right" />
+          </BrowserRouter>
+        </AuthProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )

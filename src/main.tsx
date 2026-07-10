01e27565654d76198eba6b28efdc1df98188e3d8
Rishu123-png <jaswarrishu642@gmail.com>
2026-07-10 14:39:05 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import { ThemeProvider } from './contexts/ThemeContext'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { SchoolProvider } from './contexts/SchoolContext'
 import { Toaster } from 'sonner'
 
-const qc = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { staleTime: 1000 * 60 * 2, retry: 1 }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={qc}>
-      <ThemeProvider>
-        <AuthProvider>
-          <BrowserRouter>
-            <App />
-            <Toaster richColors position="top-right" />
-          </BrowserRouter>
-        </AuthProvider>
-      </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider>
+          <AuthProvider>
+            <SchoolProvider>
+              <App />
+              <Toaster richColors position="top-right" />
+            </SchoolProvider>
+          </AuthProvider>
+        </ThemeProvider>
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
 )

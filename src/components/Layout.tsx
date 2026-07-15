508c1756ec6670e6084b4012528f58b84151823b
Rishu123-png <jaswarrishu642@gmail.com>
2026-07-15 16:06:49 +0530

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,20 +2,24 @@ import { Outlet } from 'react-router-dom'
 import Sidebar from './Sidebar'
 import Topbar from './Topbar'
 import BottomNav from './mobile/BottomNav'
+import AmbientBackground from './mobile/AmbientBackground'
+import FloatingAIAssistant from './mobile/FloatingAIAssistant'
 import { AnimePageTransition } from './AnimeWrapper'
 
 export default function Layout() {
   return (
-    <div className="min-h-screen bg-[#fcfcfc] dark:bg-zinc-950 flex">
+    <div className="app-shell min-h-screen bg-[#fcfcfc] dark:bg-zinc-950 flex">
+      <AmbientBackground />
       <Sidebar />
-      <div className="flex-1 min-w-0 flex flex-col">
+      <div className="relative z-[1] flex-1 min-w-0 flex flex-col">
         <Topbar />
-        <main className="px-4 md:px-7 py-5 md:py-7 max-w-7xl w-full mx-auto page-container pb-24 md:pb-7">
+        <main className="mobile-main px-4 md:px-7 py-5 md:py-7 max-w-7xl w-full mx-auto page-container pb-28 md:pb-7">
           <AnimePageTransition>
             <Outlet />
           </AnimePageTransition>
         </main>
       </div>
+      <FloatingAIAssistant />
       <BottomNav />
     </div>
   )

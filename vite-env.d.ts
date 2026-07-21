11687b929c45a4835853e690dd2ae4a27cd5c0d9
Rishu123-png <jaswarrishu642@gmail.com>
2026-07-21 20:28:13 +0530

    Update vite-env.d.ts

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -9,7 +9,12 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string
   readonly VITE_FIREBASE_APP_ID: string
   readonly VITE_FIREBASE_MEASUREMENT_ID: string
+  readonly VITE_AI_PROVIDER?: string
+  readonly VITE_WHATSAPP_API_KEY?: string
+  readonly VITE_GEMINI_API_KEY?: string
+  readonly VITE_GEMINI_MODEL?: string
 }
+
 interface ImportMeta {
   readonly env: ImportMetaEnv
 }
